0b1d7dc1c66b3d1cfe7ccdc6b60f8f95cc7f7cae
Nick McClorey <mcclorns@mail.uc.edu>
2018-12-06 00:36:27 -0500

    Add ability to click senate and press escape key to get out of popups

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import time
 import json
 from webDriver import executeGameSession
 
+
 file = open('settings.json', 'r')
 settings = json.loads(file.read())
 file.close()
-
 executeGameSession(settings['player'])
+print('succesfully reaped resources')
--- webDriver.py
+++ webDriver.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 # logs in, manages the game and closes the browser
@@ -8,6 +9,7 @@ def executeGameSession(player):
     # setup web browser
     browser = webdriver.Chrome('C:\\Users\\Nick\\AppData\\Local\\Programs\\chromeDriver\\chromedriver.exe')
     loginAndSelectWorld(browser, player)
+    manageSenate(browser)
     reapVillages(browser)
 
     browser.quit()
@@ -18,6 +20,8 @@ def reapVillages(browser):
     #go to island view
     goToIslandViewButton = browser.find_element_by_class_name('island_view')
     goToIslandViewButton.click()
+    time.sleep(1)
+    browser.find_element_by_tag_name('body').send_keys(Keys.ESCAPE)
     time.sleep(1)
 
     numVillagesReaped = 0
@@ -32,7 +36,7 @@ def reapVillages(browser):
         if len(disabledBanners) > 0:
             # click on button to collect resources
             collectResourcesButtons = browser.find_elements_by_class_name('card_click_area')
-            collectResourcesButtons[0].click()
+            collectResourcesButtons[1].click()
             time.sleep(1)
 
             # close village window
@@ -65,4 +69,18 @@ def loginAndSelectWorld(browser, player):
     index = player['worldIndex']
     worldButton = browser.find_elements_by_class_name('world_name')[index]
     worldButton.find_element_by_css_selector('div').click()
-    time.sleep(1.5)
+    time.sleep(2)
+    # exit any pop ups
+    browser.find_element_by_tag_name('body').send_keys(Keys.ESCAPE) 
+    time.sleep(1)
+    browser.find_element_by_tag_name('body').send_keys(Keys.ESCAPE)
+
+def manageSenate(browser):
+    # go to the city overview
+    browser.find_element_by_class_name('city_overview').click()
+    time.sleep(1)
+    
+    warCoinBox = browser.find_element_by_class_name('war_coins_box')
+    actions = ActionChains(browser)
+    actions.move_to_element(warCoinBox).move_by_offset(-440, 0).click().perform()
+    time.sleep(0.4)
